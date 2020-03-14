e524280815778826d0a5543aef84fb3fb0bf84df
Fady Soliman <fady.soliman@sitereq.com>
2020-03-14 20:02:22 +0200

    using react-native-material-dropdown

---
+++ src/api/api.js
@@ -0,0 +1,5 @@
+import supportTypes from '../data/dropdown-data.json';
+
+export const getSupportTypes = () => {
+    return supportTypes;
+};
--- src/screens/plugins/ReactNativeMaterialDropdown.js
+++ src/screens/plugins/ReactNativeMaterialDropdown.js
@@ -1,11 +1,38 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import {Dropdown} from 'react-native-material-dropdown';
+
+import {getSupportTypes} from '../../api/api';
 
 export default class ReactNativeMaterialDropdownScreen extends Component {
+
+    state = {supportTypes: []};
+
+    static navigationOptions = {
+        title: 'React Native Material Dropdown'
+    };
+
+    componentDidMount() {
+        const types = getSupportTypes();
+        this.setState({ ...this.state, supportTypes: types });
+    }
+
+    renderDropdown() {
+        console.log('data:', this.state.supportTypes);
+        if(this.state.supportTypes && this.state.supportTypes.length > 0) {
+            return (
+                <Dropdown
+                    label="Support Types"
+                    data={this.state.supportTypes}
+                />
+            );
+        }
+    }
+
     render() {
         return (
             <View>
-                <Text>This is ReactNativeMaterialDropdownScreen</Text>
+               {this.renderDropdown()}
             </View>
         );
     }
